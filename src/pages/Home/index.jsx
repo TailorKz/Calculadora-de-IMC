30bd708305305c67fd0e95fa6aec136ab00912e2
MitsukiKz <tailorkunz@gmail.com>
2024-09-05 16:52:05 -0300

    Finalizando

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,32 +1,64 @@
 import React, { useState } from 'react';
-import './style.css'
-import Trash from '../../assets/trash.svg'
+import './style.css';
+import Trash from '../../assets/trash.svg';
 
 function Home() {
-
-
-  let [Peso, setPeso] = useState(0);
-  let [Altura, setAltura] = useState(0);
+  let [Peso, setPeso] = useState('');
+  let [Altura, setAltura] = useState('');
+  let [valorIMC, setValorIMC] = useState('');
+  let [mensagemIMC, setMensagemIMC] = useState('');
 
   const CalculaImc = () => {
-    const soma = Peso / Altura * Altura
-    if (soma <= 17 ) {
-      return (
-        <p>Magro</p>
-      )
+    const pesoNum = Number(Peso);
+    const alturaNum = Number(Altura);
+
+    if (pesoNum > 0 && alturaNum > 0) {
+      const soma = pesoNum / (alturaNum * alturaNum);
+      setValorIMC(soma.toFixed(2));
+
+      if (soma <= 18) {
+        setMensagemIMC('Magreza');
+      } else if (soma > 18 && soma <= 25) {
+        setMensagemIMC('Normal');
+      } else if (soma > 25 && soma <= 30) {
+        setMensagemIMC('Sobrepeso');
+      } else if (soma > 30 && soma <= 40) {
+        setMensagemIMC('Obesidade');
+      } else {
+        setMensagemIMC('Obesidade grave');
+      }
     } else {
-      return (
-    <p>Normal</p>
-      )
+      setMensagemIMC('Preencha os campos corretamente');
     }
-  }
+  };
+
+  const handleReset = () => {
+    setPeso('');
+    setAltura('');
+    setMensagemIMC('');
+    setValorIMC('');
+  };
 
   return (
     <div className='container'>
       <form>
         <h1>Calculadora de IMC</h1>
-        <input type="number" name="Peso" placeholder='Digite seu peso (kg)' id="container--peso" onChange={evento => setPeso(evento.target.value)} />
-        <input type="number" name="Altura" placeholder='Digite sua altura (ex: 1.80 )' id="container--altura" onChange={evento => setAltura(evento.target.value)} />
+        <input
+          type="number"
+          name="Peso"
+          placeholder='Digite seu peso (kg)'
+          id="container--peso"
+          value={Peso}
+          onChange={evento => setPeso(evento.target.value)}
+        />
+        <input
+          type="number"
+          name="Altura"
+          placeholder='Digite sua altura (ex: 1.80 )'
+          id="container--altura"
+          value={Altura}
+          onChange={evento => setAltura(evento.target.value)}
+        />
         <button type="button" onClick={CalculaImc}>Calcular</button>
       </form>
       <div>
@@ -38,17 +70,16 @@ function Home() {
             <p>Altura: {Altura}m </p>
           </div>
           <div>
-            <p>Resultado: </p>
-            <button id="form-button">
+            <p>Resultado: {valorIMC} </p>
+            <p>{mensagemIMC}</p>
+            <button id="form-button" onClick={handleReset}>
               <img src={Trash} alt="Trash Icon" />
             </button>
           </div>
         </div>
       </div>
-      {CalculaImc()}
     </div>
-)
+  );
 }
 
-
-export default Home
+export default Home;
